553c1ec021bef55d0802af39c7ec9d7ce5147d90
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-07-25 13:32:40 -0500

    more style, cleaned code, all api implimented

--- client/app/app.js
+++ client/app/app.js
@@ -13,30 +13,26 @@ myApp.config(function ($routeProvider, $locationProvider) {
         .when("/users", {
             templateUrl: "views/users.html"
         })
-        .when("/user", {
+        .when("/user/:user", {
             templateUrl: "views/user.html"
         })
+
 });
-myApp.controller('allGetController', function ($scope, $http, $location, $routeParams) {
+myApp.controller('allGetController', function ($scope, $http, $location, $routeParams, $route) {
     $http.get("http://localhost:3000/api/chirps")
         .then(function (response) {
-            $scope.allChirps = response.data
+            $scope.allChirps = response.data;
+
         })
-
-
-
     $scope.insertdata = function () {
-        console.log('clicked')
         $http.post("http://localhost:3000/api/chirps", { 'user': $scope.user, 'message': $scope.message })
             .success(function (data, status, headers, config) {
                 $scope.user = ''
                 $scope.message = ''
-
+                $route.reload();
             });
         $http.get("http://localhost:3000/api/chirps")
-
             .then(function (response) {
-                console.log('got')
                 $scope.allChirps = response.data;
             })
     }
@@ -46,7 +42,6 @@ myApp.controller('allGetController', function ($scope, $http, $location, $routeP
                 $http.get("http://localhost:3000/api/chirps")
                     .then(function (response) {
                         $scope.allChirps = response.data;
-                        console.log('deleted')
                     })
             });
     }
@@ -58,57 +53,53 @@ myApp.controller('allGetController', function ($scope, $http, $location, $routeP
             })
     }
 });
-
 myApp.controller('UserController', function ($scope, $http, $location, $routeParams) {
     $http.get("http://localhost:3000/api/users")
         .then(function (response) {
-            $scope.allUsers = response.data
-            console.log(response.data)
-
+            $scope.allUsers = response.data;
         })
     $scope.goToUser = function (user) {
-
-        console.log('gotoworked')
         $http.get('/api/chirps/')
             .then(function () {
                 $location.path('/user/' + user)
-                console.log(user)
             })
-
-
     }
-
+    $scope.deleteSingleUser = function (user) {
+        $http.delete('/api/chirps/user/' + user)
+        $http.get("http://localhost:3000/api/users")
+            .then(function (response) {
+                $location.path("/users/")
+            })
+    }
 })
 myApp.controller('oneUserController', function ($scope, $routeParams, $http, $location) {
     var currentUser = $routeParams.user;
-    console.log(currentUser)
     $http.get("http://localhost:3000/api/chirps/user/" + currentUser)
         .then(function (response) {
             $scope.thisUser = response.data;
-            console.log(response)
-            console.log(response.data)
         })
 })
 myApp.controller('oneController', function ($scope, $routeParams, $http, $location) {
     var currentId = $routeParams.id;
-    console.log(currentId)
     $http.get("http://localhost:3000/api/chirps/one/" + currentId)
         .then(function (response) {
             $scope.thisChirp = response.data;
-            console.log(location.hash)
-            console.log(response.data)
         })
     $scope.deleteSingle = function (id) {
-    $http.delete('/api/chirps/one/' + id )
-
-    .success(function (data, status, headers, config) {
-            $http.get("http://localhost:3000/api/chirps")
-                .then(function (response) {
-                    console.log('working')
-                    $location.path("/list/")
-                })
-        });
-     }
+        $http.delete('/api/chirps/one/' + id)
+            .success(function (data, status, headers, config) {
+                $http.get("http://localhost:3000/api/chirps")
+                    .then(function (response) {
+                        $location.path("/list/")
+                    })
+            });
+    }
 });
-
-
+function handleSelect(elm) {
+    if (elm.value == 'Users') {
+        window.location = '#/users/'
+    }
+    else {
+        window.location = '#/user/' + elm.value;
+    }
+}
